c2b003af092f828c307d0d3cb21e87ca0d5ccb72
Megaremzik <megaremzik@gmail.com>
2018-10-16 09:43:24 +0300

    Added usage of dropzone(downloading to the webroot)

--- WebStorage/WS.Web/Controllers/HomeController.cs
+++ WebStorage/WS.Web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WS.Web.Models;
 
@@ -15,6 +19,27 @@ namespace WS.Web.Controllers
             return View();
         }
 
+        private IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(IFormFile file)
+        {
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+
+            if (file.Length > 0)
+            {
+                using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+            return RedirectToAction("Index");
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
