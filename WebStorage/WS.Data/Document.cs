e51af69a0e49c7bff32f2754c7a1ed4d03523c35
Megaremzik <megaremzik@gmail.com>
2018-10-25 22:50:52 +0300

    Add column Extention to the Document Table + changed Models for this
    #16

--- WebStorage/WS.Data/Document.cs
+++ WebStorage/WS.Data/Document.cs
@@ -12,7 +12,7 @@ namespace WS.Data
         public int Size { get; set; }
         public int ParentId { get; set; }
         public bool IsFile { get; set; }
-
+        public string Extention { get; set; }
         public virtual DocumentLink DocumentLink { get; set; }
         public virtual ICollection<UserDocument> UserDocuments { get; set; }
         public virtual User User { get; set; }
