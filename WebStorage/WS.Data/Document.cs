59aee7669f08a23962cedc068f508be89381adcf
Natalya <litovchenko.natalja@gmail.com>
2018-11-13 16:35:47 +0200

    Registration web

--- WebStorage/WS.Data/Document.cs
+++ WebStorage/WS.Data/Document.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WS.Data
 {
     public class Document
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
         public string UserId { get; set; }
         public string Name { get; set; }
