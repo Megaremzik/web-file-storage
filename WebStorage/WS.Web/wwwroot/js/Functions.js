74835587551415865c274c43e97f1efc26662d97
Evgeniy Velegura <evgeniy.velegura@gmail.com>
2018-12-04 11:05:48 +0200

    add cencel-download button

--- WebStorage/WS.Web/wwwroot/js/Functions.js
+++ WebStorage/WS.Web/wwwroot/js/Functions.js
@@ -531,5 +531,6 @@ function HideResults() {
 function StoreUserToSession() {
     sessionStorage.setItem("user", document.getElementById("email").value)
 }
+
 window.currentProcessingItem = 0;
 window.ProcessingItems = 0;
